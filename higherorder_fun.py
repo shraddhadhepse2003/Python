43fcd99bf22ae13bc6976418994cb5f2f51bcd42
shraddhadhepse2003 <shraddhadhepse2003@gmail.com>
2024-06-21 18:06:09 +0530

    'ss'

---
+++ higherorder_fun.py
@@ -0,0 +1,43 @@
+#higher order function
+##A function which takes another as an argument is called as higher order function
+##
+##Built in higher order function
+##1)filter function
+##2)map function
+##3)reduce function
+
+
+
+
+##number=[11,22,33,44,55,66]
+##def even_fun(x):
+##    if x%2==0:
+##        return True
+##filter_object=filter(even_fun,number)
+##print(filter_object)
+
+##number=[11,22,33,44,55,66]
+##def even_fun(x):
+##    if x%2==0:
+##        return True
+##filter_object=filter(even_fun,number)
+##print(list(filter_object))
+
+##number=[11,22,33,44,55,66]
+##def even_fun(x):
+##    if x%2==0:
+##        return True
+##filter_object=list(filter(even_fun,number))
+##print(filter_object)
+##print(filter_object)
+
+
+number=[45,77,88,33,48,50,13]
+def prime_fun(x):
+    for i in range(2,100,1):
+        if x%i != 0:
+            return True
+filter_object=list(filter(prime_fun,number))
+print(filter_object)
+
+
---
+++ lambda.py
@@ -0,0 +1,49 @@
+#lambda function=a function without name is called as lambda function
+#it is also called anonymous function
+#lambda function always start with lambda keyword
+
+#syntax=lambda arg_list:expression
+
+
+#example
+##a=lambda x:print(x)
+##a(4)
+
+##a=lambda x:x**2
+##print(a(4))
+
+#add two numbers
+##add=lambda x,y:print(x+y)
+##add(3,4)
+
+##def add(x,y):
+##    return x+y
+##print(add(3,4))
+
+#default return available after semicolon
+##add=lambda x,y:x+y
+##print(add(6,4))
+
+##result=lambda x,y:(x+y,x-y)
+##print(result(6,4))
+
+##result=lambda x,y:(x+y,x-y)
+##add,sub=result(6,4)
+##print(add)
+##print(sub)
+
+##add=lambda y,x:x-y
+##print(add(x=3,y=9))
+
+##add=lambda x,y=3:x+y
+##print(add(4))
+
+##add=lambda x=6,y=3:x+y
+##print(add())
+
+
+
+
+
+
+
